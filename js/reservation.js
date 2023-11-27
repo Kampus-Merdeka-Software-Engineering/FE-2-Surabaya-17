efe9d190db3790f8fd68b0b324967e5b3d77f130
Falsha Satria Dwiputra <falshasatriadwi@gmail.com>
2023-11-27 12:31:20 +0700

    Reservation

---
+++ js/reservation.js
@@ -0,0 +1,17 @@
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var dateElem = document.querySelector("#date");
+
+dateElem.setAttribute("min", dateTomorrow);
+
