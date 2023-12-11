8a068d0330a10b9f6fab5def6c6c4c720f4f5c20
MarwanMaulana <marwan.maulana023@gmail.com>
2023-12-11 21:45:51 +0700

    Update

--- js/home.js
+++ js/home.js
@@ -101,25 +101,3 @@ renderMenu();
 };
 
 renderFood();
-
-async function createReservation() {
-  const name = document.getElementById('name').value;
-  const phone = document.getElementById('phone').value;
-  const time = document.getElementById('time').value;
-  const date = document.getElementById('date').value;
-  const people = document.getElementById('people').value;
-  const branch = document.getElementById('branch-selection').value;
-  const subject = document.getElementById('message').value;
-
-  try {
-    const response = await fetch(`https://be-2-surabaya-17-production.up.railway.app/api/reservation`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ name, phone, time, date, people, branch, subject })
-    })
-  } catch(err) {
-    console.error(err);
-  }
-}
--- js/reservation.js
+++ js/reservation.js
@@ -15,3 +15,25 @@ var dateElem = document.querySelector("#date");
 
 dateElem.setAttribute("min", dateTomorrow);
 
+async function createReservation() {
+  const name = document.getElementById('name').value;
+  const phone = document.getElementById('phone').value;
+  const time = document.getElementById('time').value;
+  const date = document.getElementById('date').value;
+  const people = document.getElementById('people').value;
+  const branch = document.getElementById('branch-selection').value;
+  const subject = document.getElementById('message').value;
+
+  try {
+    const response = await fetch(`https://be-2-surabaya-17-production.up.railway.app/api/reservations`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, phone, time, date, people, branch, subject })
+    })
+    Swal.fire("Reservation Created Successfully!");
+  } catch(err) {
+    console.error(err);
+  }
+}
